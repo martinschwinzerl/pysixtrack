b322b473c91972bf27883a7f1721769676d95425
Martin Schwinzerl <martin.schwinzerl@lefou.at>
2020-01-14 11:59:09 +0100

    moves constants to MathlibDefault and provides an optional mpmath based implementation

--- pysixtrack/mathlibs.py
+++ pysixtrack/mathlibs.py
@@ -1,98 +1,74 @@
-import numpy as np
 from importlib import util
-import types
+import numpy as np
 from scipy.special import wofz
 
+
 class MathlibDefault(object):
+    from numpy import sqrt, exp, sin, cos, abs, pi, tan, sign
+    real_type = property(lambda: np.float_)
+    int_type = property(lambda: np.int64)
 
-    from numpy import sqrt, exp, sin, cos, abs, pi, tan
+    clight = property(lambda: np.float_(299792458.0))
+    echarge = property(lambda: np.float_(1.602176565e-19))
+    emass = property(lambda: np.float_(0.510998928e6))
+    nmass = property(lambda: np.float_(931.49410242e6))
+    pmass = property(lambda: np.float_(938.272081e6))
+    anumber = property(lambda: np.float_(6.02214129e23))
+    kbolz = property(lambda: np.float_(1.3806488e-23))
+    epsilon0 = property(lambda: np.float_(8.854187817e-12))
+    mu0 = property(lambda: np.float(4e-7) * np.pi)
 
-    @classmethod
-    def wfun(cls, z_re, z_im):
+    @property
+    def eradius(self):
+        return self.echarge * self.echarge / \
+            (4 * self.pi * self.epsilon0 * self.emass * self.clight * self.clight)
+
+    @property
+    def pradius(self):
+        return self.echarge * self.echarge / \
+            (4 * self.pi * self.epsilon0 * self.pmass * self.clight * self.clight)
+
+    @staticmethod
+    def wfun(z_re, z_im):
         w = wofz(z_re + 1j * z_im)
         return w.real, w.imag
 
 
-class MathlibConstants(object):
-    @staticmethod
-    def REAL_TYPE(mathlib=MathlibDefault):
-        if not( mpmath_spec is None ) and \
-            isinstance( mathlib, types.ModuleType ) and \
-            mathlib.__name__ == 'mpmath':
-            from mpmath import mp
-            return mp.mpf
-        else:
-            return np.float64
+if not util.find_spec("mpmath") is None:
+    import mpmath
 
-    @staticmethod
-    def INT_TYPE(mathlib=MathlibDefault):
-        return np.int64
+    class MpMathLib(object):
+        from mpmath import sqrt, exp, sin, cos, tan, fabs as abs, pi, sign, erfc
+        real_type = property(lambda: mpmath.mpf)
+        int_type = property(lambda: np.int64)
+
+        clight = property(lambda: mpmath.mpf("299792458.0"))
+        echarge = property(lambda: mpmath.mpf("1.602176565e-19"))
+        emass = property(lambda: mpmath.mpf("0.510998928e6"))
+        nmass = property(lambda: mpmath.mpf("931.49410242e6"))
+        pmass = property(lambda: mpmath.mpf("938.272081e6"))
+        anumber = property(lambda: mpmath.mpf("6.02214129e23"))
+        kbolz = property(lambda: mpmath.mpf("1.3806488e-23"))
+        epsilon0 = property(lambda: mpmath.mpf("8.854187817e-12"))
+        mu0 = property(lambda: mpmath.mpf("4e-7") * mpmath.pi)
+
+        @property
+        def eradius(self):
+            return self.echarge * self.echarge / \
+                (4 * self.pi * self.epsilon0 * self.emass * self.clight * self.clight)
+
+        @property
+        def pradius(self):
+            return self.echarge * self.echarge / \
+                (4 * self.pi * self.epsilon0 * self.pmass * self.clight * self.clight)
+
+        @staticmethod
+        def wfun(z_re, z_im):
+            z = mpmath.mpc(z_re, z_im)
+            w = mpmath.exp(-z * z) * mpmath.erfc(mpmath.mpc(z.imag, -z.real))
+            return w.real, w.imag
 
 
-    def __init__(self, mathlib=MathlibDefault,
-            real_type=np.float64, int_type=np.int64 ):
-        self.CLIGHT_STR = "299792458.0"
-        self.ECHARGE_STR = "1.602176565e-19"
-        self.EMASS_STR = "0.510998928e6"
-        self.NMASS_STR = "931.49410242e6"
-        self.PMASS_STR = "938.272081e6"
-        self.EPSILON0_STR = "8.854187817e-12"
-        self.ANUMBER_STR = "6.02214129e23"
-        self.KBOLZ_STR = "1.3806488e-23"
-
-        mpmath_spec = util.find_spec("mpmath")
-
-        if mpmath_spec is not None and \
-            isinstance( mathlib, types.ModuleType ) and \
-            mathlib.__name__ == 'mpmath':
-            from mpmath import mp, pi as mp_pi
-            real_type = mp.mpf
-            self.PI = mp_pi
-        else:
-            self.PI = np.pi
-
-        self.ZERO     = real_type("0.0")
-        self.ONE      = real_type("1.0")
-        self.TWO      = real_type("2.0")
-        self.ONE_HALF = real_type("0.5")
-        self.CLIGHT   = real_type(self.CLIGHT_STR)
-        self.ECHARGE  = real_type(self.ECHARGE_STR)
-        self.EMASS    = real_type(self.EMASS_STR)
-        self.NMASS    = real_type(self.NMASS_STR)
-        self.PMASS    = real_type(self.PMASS_STR)
-        self.EPSILON0 = real_type(self.EPSILON0_STR)
-        self.ANUMBER  = real_type(self.ANUMBER_STR)
-        self.KBOLZ    = real_type(self.KBOLZ_STR)
-
-        self._m = mathlib
-        self._real_type = real_type
-        self._int_type = int_type
-
-    @property
-    def real_type(self):
-        return self._real_type
-
-    @property
-    def int_type(self):
-        return self._int_type
-
-    @property
-    def m(self):
-        return self._m
-
-    @property
-    def MU0(self):
-        return self._real_type("4e-7") * self.PI
-
-    @property
-    def ERADIUS(self):
-        return self.ECHARGE * self.ECHARGE / ( self._real_type( 4 ) *
-            self.PI * self.EPSILON0 * self.EMASS * self.CLIGHT * self.CLIGHT )
-
-    @property
-    def PRADIUS(self):
-        return self.ECHARGE * self.ECHARGE / ( self._real_type( 4 ) *
-            self.PI * self.EPSILON0 * self.PMASS * self.CLIGHT * self.CLIGHT )
-
-    def sign(self, val):
-         return self.TWO * self._real_type( self._real_type(val) >= self.ZERO ) - self.ONE
+else:
+    class MpMathLib(object):
+        pass
