f4af97f043c05f0ed5ed07c8c284e4c3270f3215
Martin Schwinzerl <martin.schwinzerl@lefou.at>
2020-01-15 15:11:05 +0100

    moves mpmath implementation out of mathlibs.py, adds generic type conversion routine

--- pysixtrack/mathlibs.py
+++ pysixtrack/mathlibs.py
@@ -1,10 +1,10 @@
-from importlib import util
 import numpy as np
 from scipy.special import wofz
 
 
 class MathlibDefault(object):
     from numpy import sqrt, exp, sin, cos, abs, pi, tan, sign
+
     real_type = property(lambda: np.float_)
     int_type = property(lambda: np.int64)
 
@@ -34,41 +34,26 @@ class MathlibDefault(object):
         return w.real, w.imag
 
 
-if not util.find_spec("mpmath") is None:
-    import mpmath
+def convert_attr(attr, dtype=np.float64):
+    """Casts the attribute attr to type dtype
 
-    class MpMathLib(object):
-        from mpmath import sqrt, exp, sin, cos, tan, fabs as abs, pi, sign, erfc
-        real_type = property(lambda: mpmath.mpf)
-        int_type = property(lambda: np.int64)
+    Keyword arguments:
+    attr -- attribute to be cast. scalar, string or iterable object assumed
+    dtype -- type alias to be used for casting
+       If attr is iterable, this is done for each element in the sequence
 
-        clight = property(lambda: mpmath.mpf("299792458.0"))
-        echarge = property(lambda: mpmath.mpf("1.602176565e-19"))
-        emass = property(lambda: mpmath.mpf("0.510998928e6"))
-        nmass = property(lambda: mpmath.mpf("931.49410242e6"))
-        pmass = property(lambda: mpmath.mpf("938.272081e6"))
-        anumber = property(lambda: mpmath.mpf("6.02214129e23"))
-        kbolz = property(lambda: mpmath.mpf("1.3806488e-23"))
-        epsilon0 = property(lambda: mpmath.mpf("8.854187817e-12"))
-        mu0 = property(lambda: mpmath.mpf("4e-7") * mpmath.pi)
+    Returns:
+    attr after the conversion
 
-        @property
-        def eradius(self):
-            return self.echarge * self.echarge / \
-                (4 * self.pi * self.epsilon0 * self.emass * self.clight * self.clight)
+    Throws:
+    nothing
 
-        @property
-        def pradius(self):
-            return self.echarge * self.echarge / \
-                (4 * self.pi * self.epsilon0 * self.pmass * self.clight * self.clight)
-
-        @staticmethod
-        def wfun(z_re, z_im):
-            z = mpmath.mpc(z_re, z_im)
-            w = mpmath.exp(-z * z) * mpmath.erfc(mpmath.mpc(z.imag, -z.real))
-            return w.real, w.imag
-
-
-else:
-    class MpMathLib(object):
-        pass
+    """
+    try:
+        it_range = iter(attr)
+    except TypeError:
+        attr = dtype(attr)
+    else:
+        for it in it_range:
+            it = dtype(it)
+    return attr
